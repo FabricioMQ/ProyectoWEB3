73569efedee46416c8d0f083c35346b657adb815
FabricioMQ <fabriciomq1@gmail.com>
2023-03-30 00:08:51 -0600

    API CLINICA V0.2.1

--- Middlewares/Validar_Role.js
+++ Middlewares/Validar_Role.js
@@ -7,22 +7,23 @@ const Roles = {
 };
 
 const RequiereRole = (...roles) => {
-    if (!req.usuario) {
-        return res.status(500).json({
-            ok: false,
-            msg: "No se puede verificar el rol sin validar el token primero"
-        });
+    return (req=request, res=response, next)=>{
+        if (!req.usuario) {
+            return res.status(500).json({
+                ok: false,
+                msg: "No se puede verificar el rol sin validar el token primero"
+            });
+        }
+        const isInclude = roles.includes(req.Rol);
+        if (!isInclude) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Usuario no autorizado'
+            });
+        }
+        next();
     }
-    const isInclude = roles.includes(req.Rol);
-    if (!isInclude) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Usuario no autorizado'
-        });
-    }
-    next();
 }
-
 module.exports = {
     Roles,
     RequiereRole
