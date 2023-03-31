820ac5f67d4e3b335faed2eb25109cf784c7c79f
FabricioMQ <fabriciomq1@gmail.com>
2023-03-30 23:06:33 -0600

    API CLINICA V0.4.7

--- Middlewares/Validar_Cita.js
+++ Middlewares/Validar_Cita.js
@@ -1,6 +1,6 @@
 const Cita=require('../Models/Citas');
 const Valida_Citas = async (req, res) => {
-    return Cita.exists({ 'Hora': req.Hora, 'Medico': { $regex: new RegExp(req.Medico.replace(/\s/g, ''), 'i') }, 'Fecha': req.Fecha }).then(cita => {
+    return Cita.exists({ 'Hora': req.Hora, 'Medico':  { $regex: new RegExp(req.Medico, 'i') }, 'Fecha': req.Fecha }).then(cita => {
       if (cita) {
         return Promise.reject(`No hay cita para esta fecha y hora con el medico `);
       } else { 
